ecf352966ee084a0889c18f044a42dc751e1f79b
Anais <anaiszed447@gmail.com>
2024-07-31 18:15:50 -0400

    Implemented add item functionality

--- app/page.js
+++ app/page.js
@@ -24,6 +24,23 @@ export default function Home() {
  
   }
 
+  //add item function
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+       const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+
+    await updateInventory()
+  }
+
+  //remove item function
   const removeItem = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
