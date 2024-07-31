c3c6f0d21b6171cdd5cf31775e03108b37ea4818
Anais <anaiszed447@gmail.com>
2024-07-31 17:07:52 -0400

    Queried and printed the inventory items from firebase database

--- app/page.js
+++ app/page.js
@@ -1,8 +1,47 @@
+'use client'
 import Image from "next/image";
-
+import { useState, useEffect } from "react";
+import {firestore} from '@/firebase'
+import { Box, Typography } from "@mui/material";
+import { collection, query, getDocs } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+ 
+  }
+
+  useEffect(() => {
+    updateInventory()
+  },[])
+
   return (
-    <>Hello World</>
+    <Box>
+      <Typography variant="h1"> Inventory Management </Typography>
+      {
+        inventory.forEach((item) => {
+          console.log(item)
+          return(
+            <>
+            {item.name}
+            {item.count}
+            </>
+          )
+        })
+      }
+    </Box>
   );
 }
